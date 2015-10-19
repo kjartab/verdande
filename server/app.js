0829a4177dbcc34a1a6b92c346322f9c25892cec
Kjartan Bjørset <kjartanbjorset@gmail.com>
2015-10-19 22:52:35 +0200

    basic setup

---
+++ server/app.js
@@ -0,0 +1,31 @@
+var net = require('net');
+
+
+var server = net.createServer(function (conn) {
+    console.log("Server: Node client connected");
+
+    conn.on('error', function(err){
+
+        console.log(err);    
+    });             
+
+    conn.on("end", function() { 
+        console.log('Server: Node client disconnected');
+    });
+
+    // Handle data from client
+    conn.on("data", function(data) {
+
+          try {
+            var data = JSON.parse(data);
+            console.log(data);
+          } catch (e) {
+            return console.error(e);
+          }
+
+
+    });
+
+});
+
+var clients = [];
