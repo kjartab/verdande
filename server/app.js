fc0697f315882800299af6ce93e3fc04f3f8b7d8
Kjartan Bjørset <kjartanbjorset@gmail.com>
2015-11-07 18:10:30 +0100

    t

--- server/app.js
+++ server/app.js
@@ -1,31 +1,32 @@
 var net = require('net');
+var http = require("http");
 
 
-var server = net.createServer(function (conn) {
-    console.log("Server: Node client connected");
+// var server = net.createServer(function (conn) {
+    // console.log("Server: Node client connected");
 
-    conn.on('error', function(err){
+    // conn.on('error', function(err){
 
-        console.log(err);    
-    });             
+        // console.log(err);    
+    // });             
 
-    conn.on("end", function() { 
-        console.log('Server: Node client disconnected');
-    });
+    // conn.on("end", function() { 
+        // console.log('Server: Node client disconnected');
+    // });
 
-    // Handle data from client
-    conn.on("data", function(data) {
+    // // Handle data from client
+    // conn.on("data", function(data) {
 
-          try {
-            var data = JSON.parse(data);
-            console.log(data);
-          } catch (e) {
-            return console.error(e);
-          }
+          // try {
+            // var data = JSON.parse(data);
+            // console.log(data);
+          // } catch (e) {
+            // return console.error(e);
+          // }
 
 
-    });
+    // });
 
-});
+// });
 
-var clients = [];
+// var clients = [];
